d210ca3ccb63416cd669ccc03673c67769bbb0bc
Samuel Rolo <samuelrolo@gmail.com>
2025-05-13 15:25:24 +0100

    up

--- SITE/generate_flask_key.py
+++
@@ -1,7 +0,0 @@
-import secrets
-import base64
-
-# Gerar uma chave secreta forte
-secret_key = base64.b64encode(secrets.token_bytes(32)).decode('utf-8')
-
-print(f"A sua Flask Secret Key gerada é: {secret_key}")
